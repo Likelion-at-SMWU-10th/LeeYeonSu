4b9c0be1e2cbd90924ec93c67e6b8bb9e261482a
bestlalala <bestdustn@gmail.com>
2022-02-22 14:00:11 +0900

    made Model

--- Django_part/4. 사용자 입력받기/blogproject/blogapp/models.py
+++ Django_part/4. 사용자 입력받기/blogproject/blogapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField(max_length=1000)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self): # 목록에서 내가 작성한 글의 title이 보이게 하기
+        return self.title
