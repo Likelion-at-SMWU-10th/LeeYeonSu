6b6a9f7ff39278b1d5ee346be330c3e2ceda0d96
bestlalala <bestdustn@gmail.com>
2022-02-22 14:11:36 +0900

    HTML Form

--- Django_part/4. 사용자 입력받기/blogproject/blogproject/urls.py
+++ Django_part/4. 사용자 입력받기/blogproject/blogproject/urls.py
@@ -20,4 +20,8 @@ from blogapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+
+    # html form 을 이용해 블로그 객체 만들기
+    path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
 ]
