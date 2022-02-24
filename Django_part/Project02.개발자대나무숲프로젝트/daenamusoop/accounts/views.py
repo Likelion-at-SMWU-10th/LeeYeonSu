90690f5b04437ca372331a62af7795d805da2097
bestlalala <bestdustn@gmail.com>
2022-02-24 20:51:55 +0900

    get some fix

--- Django_part/Project02.개발자대나무숲프로젝트/daenamusoop/accounts/views.py
+++ Django_part/Project02.개발자대나무숲프로젝트/daenamusoop/accounts/views.py
@@ -19,3 +19,6 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return redirect('home')
+
+def signup(request):
+    return render(request, 'register.html')
--- Django_part/Project02.개발자대나무숲프로젝트/daenamusoop/snsapp/views.py
+++ Django_part/Project02.개발자대나무숲프로젝트/daenamusoop/snsapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render, get_object_or_404
-from .forms import PostForm, CommentForm
-from .models import Post
+from .forms import PostForm, CommentForm, FreePostForm, FreeCommentForm
+from .models import Post, FreePost
 
 def home(request):
     # posts = Post.objects.all()
@@ -35,3 +35,35 @@ def new_comment(request, post_id):
         finished_form.post = get_object_or_404(Post, pk=post_id)
         finished_form.save()
     return redirect('detail', post_id)
+
+def freehome(request):
+    # posts = Post.objects.all()
+    freeposts = FreePost.objects.filter().order_by('-date')
+    return render(request, 'free_index.html', {'freeposts':freeposts})
+
+# 자유 게시판
+def freepostcreate(request):
+    if request.method == 'POST' or request.method == 'FILES':
+        form = FreePostForm(request.POST, request.FILES)
+        if form.is_valid():
+            unfinished = form.save(commit=False)
+            unfinished.author = request.user        #user 추가!
+            unfinished.save()
+            return redirect('freehome')
+    else:
+        form = FreePostForm()
+    return render(request, 'free_post_form.html', {'form':form})    
+
+def freedetail(request, post_id):
+    post_detail = get_object_or_404(FreePost, pk=post_id)
+    comment_form = FreeCommentForm()
+    return render(request, 'free_detail.html', {'post_detail':post_detail, 'comment_form':comment_form})
+
+# 댓글 저장
+def new_freecomment(request, post_id):
+    filled_form = FreeCommentForm(request.POST)
+    if filled_form.is_valid():
+        finished_form = filled_form.save(commit=False) # 아직은 저장 안함
+        finished_form.post = get_object_or_404(Post, pk=post_id)
+        finished_form.save()
+    return redirect('freedetail', post_id)
