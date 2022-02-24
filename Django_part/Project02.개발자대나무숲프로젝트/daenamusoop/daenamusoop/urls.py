1bbaea469984811f7d812430a97217e3ccbc06b7
bestlalala <bestdustn@gmail.com>
2022-02-24 16:03:58 +0900

    detail page

--- Django_part/Project02.개발자대나무숲프로젝트/daenamusoop/daenamusoop/urls.py
+++ Django_part/Project02.개발자대나무숲프로젝트/daenamusoop/daenamusoop/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('postcreate', views.postcreate, name='postcreate'),
+    path('detail/<int:post_id>', views.detail, name='detail'),
 ]
--- Django_part/Project02.개발자대나무숲프로젝트/daenamusoop/snsapp/views.py
+++ Django_part/Project02.개발자대나무숲프로젝트/daenamusoop/snsapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from .forms import PostForm
 from .models import Post
 
@@ -21,4 +21,7 @@ def postcreate(request):
     else:
         form = PostForm()
     return render(request, 'post_form.html', {'form':form})    
-    
+
+def detail(request, post_id):
+    post_detail = get_object_or_404(Post, pk=post_id)
+    return render(request, 'detail.html', {'post_detail':post_detail})
